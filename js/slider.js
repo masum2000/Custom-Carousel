1eb24130d7ca91f71904f279adb5d83b5d6f1a2c
Masum <masumt999.com>
2023-03-17 12:12:22 +0600

    updated

--- js/slider.js
+++ js/slider.js
@@ -1,13 +1,13 @@
 const images = [
-    'images/1.JPG',
-    'images/2.JPG',
-    'images/3.JPG',
-    'images/4.JPG',
-    'images/5.JPG',
-    'images/6.JPG',
-    'images/7.JPG',
+    'images/1.jpg',
+    'images/2.jpg',
+    'images/3.jpg',
+    'images/4.jpg',
+    'images/5.jpg',
+    'images/6.jpg',
+    'images/7.jpg',
     'images/8.jpeg',
-    'images/9.JPG',
+    'images/9.jpg',
 ]
 
 let imgIndex = 0;
